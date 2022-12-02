226017cc182a3c3f05757ebefb8bcfaeabfb65bb
cmoisa <costi.moisa98@gmail.com>
2022-12-02 16:45:11 +0200

    Linux setup

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
-﻿internal class Program
+﻿class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-
-
-        while(true){
-            
+        
+        using (broEngine.Engine engine = new broEngine.Engine(800, 600, "BroEngine") )
+        {
+            engine.Run(60.0);
         }
     }
 }
